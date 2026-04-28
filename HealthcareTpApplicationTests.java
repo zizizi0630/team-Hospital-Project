4134e92521efe45dd605ab34361452f2771eb78d
zizizi0630 <zizizi0630@naver.com>
2026-04-28 13:05:22 +0900

    Add files via upload

---
+++ HealthcareTpApplicationTests.java
@@ -0,0 +1,13 @@
+package healthcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcareTpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
